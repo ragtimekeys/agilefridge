0245db89efd20a75d996e6a19ef7ae15d07478b4
ragtimekeys <ragtimekeys@gmail.com>
2020-05-11 21:14:31 -0500

    Format and update project files

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,49 +8,49 @@ import (
 )
 
 const (
-	EPIC_ISSUE_TYPE = "epic"
+	EPIC_ISSUE_TYPE  = "epic"
 	STORY_ISSUE_TYPE = "story"
-	TASK_ISSUE_TYPE = "task"
+	TASK_ISSUE_TYPE  = "task"
 )
 
-type(
-	IdentifyingData struct{
-		Id string `json:"id"`
+type (
+	IdentifyingData struct {
+		Id    string `json:"id"`
 		Class string `json:"type"`
 	}
-	MutationTimestamps struct{
+	MutationTimestamps struct {
 		CreateTime time.Time `json:"createTime"`
 		UpdateTime time.Time `json:"updateTime"`
 		DeleteTime time.Time `json:"deleteTime"`
 	}
-	DescriptiveMetadata struct{
+	DescriptiveMetadata struct {
 		Title string `json:"title"`
-		Body string `json:"body"`
+		Body  string `json:"body"`
 	}
-	OwnershipMetadata struct{
-		Owner string `json:"owner"`
-		Creator string `json:"creator"`
-		Assignee string `json:"assignee"`
+	OwnershipMetadata struct {
+		Owner     string `json:"owner"`
+		Creator   string `json:"creator"`
+		Assignee  string `json:"assignee"`
 		Requestor string `json:"requestor"`
 	}
-	Issue struct{
+	Issue struct {
 		IdentifyingData
 		MutationTimestamps
 
 		ProjectId string `json:"projectId"`
-		Type string `json:"type"`
+		Type      string `json:"type"`
 
 		DescriptiveMetadata
 		OwnershipMetadata
- 	}
-	Project struct{
+	}
+	Project struct {
 		IdentifyingData
 		MutationTimestamps
 
 		DescriptiveMetadata
 		OwnershipMetadata
 	}
-	Comment struct{
+	Comment struct {
 		IdentifyingData
 		MutationTimestamps
 
@@ -60,13 +60,13 @@ type(
 		OwnershipMetadata
 	}
 
-	User struct{
+	User struct {
 		IdentifyingData
 		MutationTimestamps
 
 		DisplayName string `json:"displayName"`
-		GitHubId string `json:"gitHubId"`
-		GoogleId string `json:"googleId"`
+		GitHubId    string `json:"gitHubId"`
+		GoogleId    string `json:"googleId"`
 	}
 )
 
@@ -77,6 +77,5 @@ func main() {
 
 	http.HandleFunc("/issues", serverHandler)
 	log.Fatalln(http.ListenAndServe(":8080", nil))
-
 
 }
